dbe243fb1185bc886f48a64d8a95db45d26a137a
MaartenDeKlerkPXL <maarten.deklerk@student.pxl.be>
2026-04-13 16:01:11 +0100

    “fix-alles-werktV4”

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = 'https://lfchgbinrewruakjwunv.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_Nw69Pzdjr9H8Fv1DbPTwyw_d1Nhtzxl';
+
+const USER_SHEETS = {
+    "Maarten de Klerk": "https://script.google.com/macros/s/AKfycbyJB0k63jGd736aB3XN7a8y-HsxbFohBTMyUEwadipjKvFXTIz3xxxLutZiBJDDgg_YdA/exec",
+    "Bob": "https://script.google.com/macros/s/DE_BOB_URL/exec",
+    "Stef": "https://script.google.com/macros/s/DE_STEF_URL/exec"
+};
